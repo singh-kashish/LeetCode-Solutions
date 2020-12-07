78128ea2772a0ea1ae48b59c10c5dc0ffb1d5762
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2020-12-07 13:12:36 +0000

    Sync LeetCode submission - Reverse String (cpp)

---
+++ problems/reverse_string/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void swap(char &x,char &y){
+        char temp=x;
+        x=y;
+        y=temp;
+    }
+    void reverseString(vector<char>& s) {
+        int start=0,end=s.size()-1;
+        while(start<end){
+            swap(s[start],s[end]);
+            start++;
+            end--;
+        }
+    }
+};
