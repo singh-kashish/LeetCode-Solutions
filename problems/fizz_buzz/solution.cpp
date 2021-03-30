b9a2df786773d886739f4011078618b96e60f555
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2021-03-30 11:48:27 +0000

    Sync LeetCode submission - Fizz Buzz (cpp)

---
+++ problems/fizz_buzz/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string>arr;
+        for(int i=1;i<=n;i++){
+            if(i%3==0 && i%5==0)arr.push_back("FizzBuzz");
+            else if(i%3==0)arr.push_back("Fizz");
+            else if(i%5==0)arr.push_back("Buzz");
+            else {
+                string str=to_string(i);
+                arr.push_back(str);
+            }
+        }
+        return arr;
+    }
+};
