573ab1229cfb31a63d9bff26702f4ec3a2bf45a5
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2021-05-01 18:34:10 +0000

    Sync LeetCode submission - Search a 2D Matrix (cpp)

--- problems/search_a_2d_matrix/solution.cpp
+++ problems/search_a_2d_matrix/solution.cpp
@@ -1,13 +1,32 @@
 class Solution {
 public:
-     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-     for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size();j++){
-                if(target==matrix[i][j]){
-                    return true;    
-                }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int columns=matrix[0].size();
+        int rows = matrix.size();
+        int i = 0,j = columns-1;
+        if((rows == 1 and columns == 1)and matrix[0][0]!=target)return false;
+        if(rows==1 and columns!=1){
+            int j=0;
+            while(j<columns){
+                if(matrix[0][j++]==target)return true;
             }
+            return false;
         }
-         return false;
-     }
+        if(rows!=1 and columns==1){
+            int j=0;
+            while(j<rows){
+                if(matrix[j++][0]==target)return true;
+            }
+            return false;
+        }
+        i = 0;
+        j = columns-1;
+        while(i<rows and j>=0){
+            int value=matrix[i][j];
+            if(value==target)return true;
+            else if(value>target)j--;
+            else i++;
+        }
+        return false;
+    }
 };
