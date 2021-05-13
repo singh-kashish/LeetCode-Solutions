f67f866daa989fe952a8551d2ba2132bdf565ee6
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2021-05-13 16:56:43 +0000

    Sync LeetCode submission - Binary Search Tree to Greater Sum Tree (cpp)

---
+++ problems/binary_search_tree_to_greater_sum_tree/solution.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>inord;
+    void inorder(TreeNode *root){
+        if(root){
+        inorder(root->left);
+        inord.push_back(root->val);
+        inorder(root->right);
+        }
+    }
+    void change(TreeNode *root){
+        auto index=find(inord.begin(),inord.end(),root->val)-inord.begin();
+        int sum=0;
+        for(int i=index;i<inord.size();i++)sum+=inord[i];
+        root->val=sum;
+    }
+    void makeChange(TreeNode *root){
+        if(root){
+        change(root);
+        makeChange(root->left);
+        makeChange(root->right);
+    }}
+    TreeNode* bstToGst(TreeNode* root) {
+        inorder(root);
+        makeChange(root);
+        return root;
+    }
+};
