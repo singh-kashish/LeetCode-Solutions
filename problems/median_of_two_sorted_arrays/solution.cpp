04b9d19137c332508ad0113b29efae533311578e
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2021-07-07 17:28:26 +0000

    Sync LeetCode submission - Median of Two Sorted Arrays (cpp)

--- problems/median_of_two_sorted_arrays/solution.cpp
+++ problems/median_of_two_sorted_arrays/solution.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         if(nums1.size()>nums2.size()){
-            auto temp=nums1;
+            auto itr=nums1;
             nums1=nums2;
-            nums2=temp;
+            nums2=itr;
         }
-        int n1=nums1.size();
-        int n2=nums2.size();
-        int begin1=0,end1=n1;
-        while(begin1<=end1){
-            int i1=(begin1+end1)/2;
-            int i2=(n1+n2+1)/2-i1;
-            int min1=(i1==n1)?INT_MAX:nums1[i1];
-            int max1=(i1==0)?INT_MIN:nums1[i1-1];
-            int min2=(i2==n2)?INT_MAX:nums2[i2];
-            int max2=(i2==0)?INT_MIN:nums2[i2-1];
-            if(max1<=min2 && max2<=min1){
-                if((n1+n2)%2==0)return (double) (max(max1,max2)+min(min1,min2))/2;
+        int n1=nums1.size(),n2=nums2.size();
+        int left=0,right=nums1.size();
+        while( left <= right ){
+            int itr1 = left+(right-left)/2;
+            int itr2 = ((n1+n2+1)/2) - itr1;
+            int max1 = itr1==0 ? INT_MIN : nums1[itr1-1];
+            int max2 = itr2==0 ? INT_MIN : nums2[itr2-1];
+            int min1 = itr1==n1 ? INT_MAX : nums1[itr1];
+            int min2 = itr2==n2 ? INT_MAX : nums2[itr2];
+            if( max1<=min2 and max2<=min1 ){
+                if((n1+n2)%2==0)return (double)(max(max1,max2)+min(min1,min2))/2;
                 else return (double)max(max1,max2);
             }
-            else if(max1>min2)end1=i1-1;
-            else begin1=i1+1;
+            else if(max1>min2)right=itr1-1;
+            else left=itr1+1;
         }
-      return 1.5;  
+        return 1.5;
+        
     }
 };
