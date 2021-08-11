94c9d5cb4ea554775b20bccc806795bcfe1afc4e
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2021-08-11 17:43:50 +0000

    Sync LeetCode submission - Maximum Depth of Binary Tree (cpp)

--- problems/maximum_depth_of_binary_tree/solution.cpp
+++ problems/maximum_depth_of_binary_tree/solution.cpp
@@ -13,6 +13,6 @@ class Solution {
 public:
     int maxDepth(TreeNode* root) {
         if(root==NULL)return 0;
-        else return max(maxDepth(root->left),maxDepth(root->right))+1;
+        return 1+max(maxDepth(root->left),maxDepth(root->right));
     }
 };
