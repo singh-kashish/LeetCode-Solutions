c696d6d1deda73ee9d6875a897279014add623b3
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2021-09-02 13:04:34 +0000

    Sync LeetCode submission - Min Cost Climbing Stairs (cpp)

--- problems/min_cost_climbing_stairs/solution.cpp
+++ problems/min_cost_climbing_stairs/solution.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
+    int rec(int n,vector<int>&cost){
+        if(n==1) return min(cost[0]+cost[1],cost[1]);
+        if(n==0)return cost[0];
+        if(n==cost.size())return min(rec(n-1,cost),rec(n-2,cost));
+        return cost[n]+min(rec(n-1,cost),rec(n-2,cost));
+    }
     int minCostClimbingStairs(vector<int>& cost) {
+        //return rec(cost.size(),cost);
         int n=cost.size();
         if(n==1)return cost[0];
-        else if(n==2)return min(cost[0],cost[1]);
-        else{
-            int minCost[n];
-            minCost[n-1]=cost[n-1];
-            minCost[n-2]=cost[n-2];
-            for(int i=n-3;i>=0;i--){
-                minCost[i]=cost[i]+min(minCost[i+1],minCost[i+2]);
-            }
-            return min(minCost[0],minCost[1]);
-        }
+        if(n==2)return min(cost[0],cost[1]);
+        int arr[n];
+        arr[0]=cost[0];
+        arr[1]=cost[1];
+        for(int i=2;i<n;i++)arr[i]=cost[i]+min(arr[i-1],arr[i-2]);
+        return min(arr[n-1],arr[n-2]);
     }
 };
