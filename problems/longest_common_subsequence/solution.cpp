abb44fe6d44e20c00ddc4c60bdc047202a06e994
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2021-09-06 15:58:39 +0000

    Sync LeetCode submission - Longest Common Subsequence (cpp)

--- problems/longest_common_subsequence/solution.cpp
+++ problems/longest_common_subsequence/solution.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        int n=text2.length(),m=text1.length();
-        int table[m+1][n+1];
-        for(int i=0;i<=m;i++)table[i][0]=0;
-        for(int j=0;j<=n;j++)table[0][j]=0;
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(text1[i-1]==text2[j-1])table[i][j]=1+table[i-1][j-1];
-                else{
-                    table[i][j]=max(table[i-1][j],table[i][j-1]);
+            int n1 = text1.length() , n2 = text2.length();
+            
+            // create dp table
+            int dp[n1+1][n2+1];
+            memset(dp,0,sizeof(dp));
+            
+            //fill from 1,1
+            for(int i=1;i<=n1;i++){
+                for(int j=1;j<=n2;j++){
+                    if(text1[i-1]==text2[j-1])dp[i][j]=dp[i-1][j-1]+1;
+                    else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    
                 }
             }
-        }
-        return table[m][n];
+            return dp[n1][n2];
     }
-};
+    };
