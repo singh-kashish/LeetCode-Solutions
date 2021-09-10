7d8724a4811386bfb74df919260d888f87e3f605
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2021-09-10 17:31:07 +0000

    Sync LeetCode submission - Linked List Cycle II (cpp)

--- problems/linked_list_cycle_ii/solution.cpp
+++ problems/linked_list_cycle_ii/solution.cpp
@@ -6,22 +6,26 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+typedef ListNode Node;
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        if(!head || !(head->next))return NULL;
-        ListNode *fast=head,*slow=head;
-        while(fast && fast->next){
-            fast=fast->next->next;
-            slow=slow->next;
+        if(!head or head->next==NULL)return NULL;
+        Node *slow = head , *fast = head;
+        while(slow and fast and fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
             if(slow==fast)break;
         }
         if(slow!=fast)return NULL;
+        
+        if(slow==head and fast==head)return head;
         slow=head;
-        while(slow!=fast){
+        while(slow->next!=fast->next){
             slow=slow->next;
             fast=fast->next;
         }
-        return slow;
+        return slow->next;
+        
     }
 };
