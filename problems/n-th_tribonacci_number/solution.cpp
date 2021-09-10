caccaa237c5a3d2eece43c959bd1d3866275dbd1
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2021-09-10 20:02:36 +0000

    Sync LeetCode submission - N-th Tribonacci Number (cpp)

--- problems/n-th_tribonacci_number/solution.cpp
+++ problems/n-th_tribonacci_number/solution.cpp
@@ -8,8 +8,11 @@ public:
             dp[0]=0;
             dp[1]=1;
             dp[2]=1;
-            for(int i=3;i<=n;i++)dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+            for(int i=3;i<=n;i++){
+                dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+            }
             return dp[n];
         }
-    }
+        }
+    
 };
