9a922c267a0c542b75f9637480e3e7318d0f2828
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2022-01-03 08:52:59 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- problems/two_sum/solution.cpp
+++ problems/two_sum/solution.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int>m;
-        for(int i=0;i<nums.size();i++){
-            if(m.find((target-nums[i]))!=m.end()){
-                return {m[(target-nums[i])],i};
+        unordered_map<int,int> dict;
+        int n = nums.size(); 
+        for( int i = 0 ; i < n ; i++ ){
+            if( dict.find( target-nums[i] ) == dict.end() ){
+                dict[nums[i]] = i;
             }
-            else m[nums[i]]=i;
+            else{
+                return { dict[(target-nums[i])] , i };
+            }
         }
         return {-1,-1};
     }
