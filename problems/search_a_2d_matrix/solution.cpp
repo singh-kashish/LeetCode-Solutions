d1dab516e020af7e9cc2d874d1735c2677eb250c
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2022-01-13 09:18:19 +0000

    Sync LeetCode submission - Search a 2D Matrix (cpp)

--- problems/search_a_2d_matrix/solution.cpp
+++ problems/search_a_2d_matrix/solution.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int columns=matrix[0].size();
-        int rows = matrix.size();
-        int i = 0,j = columns-1;
-        if((rows == 1 and columns == 1)and matrix[0][0]!=target)return false;
-        if(rows==1 and columns!=1){
-            int j=0;
-            while(j<columns){
-                if(matrix[0][j++]==target)return true;
-            }
+        int i=0,j=0;
+        int rowMax = matrix.size() , colMax = matrix[0].size();
+        if(colMax==1){
+            for(auto x:matrix)if(x[0]==target)return true;
             return false;
         }
-        if(rows!=1 and columns==1){
-            int j=0;
-            while(j<rows){
-                if(matrix[j++][0]==target)return true;
-            }
-            return false;
-        }
-        i = 0;
-        j = columns-1;
-        while(i<rows and j>=0){
-            int value=matrix[i][j];
-            if(value==target)return true;
-            else if(value>target)j--;
-            else i++;
+        int rowNum = 0;
+        while(rowNum+1<rowMax and matrix[rowNum+1][0] <= target)rowNum++;
+        for(int colNum=0;colNum<colMax;colNum++){
+            if(matrix[rowNum][colNum]==target)return true;
         }
         return false;
-    }
+        }
 };
