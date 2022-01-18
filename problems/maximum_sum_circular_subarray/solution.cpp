743090e6d7e3db3c8f004d88d4414a7ddfea6ad1
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2022-01-18 16:00:03 +0000

    Sync LeetCode submission - Maximum Sum Circular Subarray (cpp)

--- problems/maximum_sum_circular_subarray/solution.cpp
+++ problems/maximum_sum_circular_subarray/solution.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int kadane(vector<int>&nums){
-        int n=nums.size();
-        int res=nums[0],currSum=nums[0];
+        int n = nums.size();
+        int currSum = nums[0] , res = nums[0];
         for(int i=1;i<n;i++){
             currSum = max(currSum+nums[i],nums[i]);
-            res=max(res,currSum);
+            res = max(res,currSum);
         }
         return res;
     }
     int maxSubarraySumCircular(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1)return nums[0];
-        int maxSumUnchangedKadane= kadane(nums);
-        int sum=0;
+        int n = nums.size();
+        int normalSum = kadane(nums);
+        if(normalSum<0)return normalSum;
+        int sum = 0;
         for(int i=0;i<n;i++){
             sum+=nums[i];
-            nums[i]= -nums[i];
+            nums[i]=-nums[i];
         }
-        if(maxSumUnchangedKadane<0)return maxSumUnchangedKadane;
-        int invertedKadaneMaxSum=kadane(nums);
-        return max(sum+invertedKadaneMaxSum,maxSumUnchangedKadane);
+        int ajeebSum = kadane(nums);
+        
+        return max(normalSum, sum+ajeebSum);
+        
     }
 };
