ea4c6338198fb2891700d99d5cf826d88558dd36
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2022-03-09 14:42:24 +0000

    Sync LeetCode submission - Longest Palindromic Subsequence (cpp)

--- problems/longest_palindromic_subsequence/solution.cpp
+++ problems/longest_palindromic_subsequence/solution.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    string reverse(string s){
-        string p;
-        for(int i=s.length()-1;i>=0;i--)p+=s[i];
-        return p;
-    }
     int longestPalindromeSubseq(string s) {
-        string rev = reverse(s);
+        string p =s;
+        reverse(p.begin(),p.end());
         int n = s.length();
-        
         int dp[n+1][n+1];
         memset(dp,0,sizeof(dp));
-        
-        for(int i=1;i<=n;i++){
+        for(int i=1 ;i<=n;i++){
             for(int j=1;j<=n;j++){
-                if(s[i-1]==rev[j-1])dp[i][j]=dp[i-1][j-1]+1;
-                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                if(s[i-1]==p[j-1])dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
             }
         }
-        return dp[n][n]; 
+        return dp[n][n];
     }
 };
