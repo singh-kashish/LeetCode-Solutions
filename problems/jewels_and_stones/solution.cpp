4666c246deb50339fe2e5641e32fb2bda16ed627
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2022-05-30 04:38:10 +0000

    Sync LeetCode submission - Jewels and Stones (cpp)

--- problems/jewels_and_stones/solution.cpp
+++ problems/jewels_and_stones/solution.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        unordered_set<char>s;
-        for(auto x:jewels){
-            s.insert(x);
-        }
+        unordered_set<char>dict;
+        for(auto x:jewels)dict.insert(x);
         int result = 0;
-        for(auto x:stones){
-            if(s.find(x)!=s.end())result++;
-        }
+        for(auto x:stones)if(dict.find(x)!=dict.end())result++;
         return result;
     }
 };
