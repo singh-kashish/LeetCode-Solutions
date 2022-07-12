688586da60ae9fc40044137f23375e6f7cc67939
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2022-07-12 13:18:16 +0000

    Sync LeetCode submission - Valid Palindrome (cpp)

--- problems/valid_palindrome/solution.cpp
+++ problems/valid_palindrome/solution.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    bool palindromeCheck(string s){
-        int l=0,r=s.length()-1;
-        while(l<=r){
-            if(s[l]!=s[r])return false;
-            else{
-                l++;
-                r--;
+    bool isPalindrome(string s) {
+        int left = 0, right=s.length()-1;
+        while(left<right){
+            while(true and left<right){
+                if( (s[left] >='a' and s[left]<='z') or ( s[left]>='A' and s[left]<='Z') or isdigit(s[left]) )break;
+                else left++;
             }
+            while(true and left<right){
+                if( (s[right] >='a' and s[right]<='z') or ( s[right]>='A' and s[right]<='Z') or isdigit(s[right])  )break;
+                else right--;
+            }
+            if(left<=right and left<=s.length() and tolower(s[left]) == tolower(s[right]) ){
+                left++;
+                right--;
+            }
+            else return false;
         }
         return true;
-    }
-    bool isPalindrome(string s) {
-        
-        string str;
-        for(auto x:s){
-            if(isalpha(x))str+=tolower(x);
-            if(isdigit(x))str+=x-'0';
-        }
-       if(str.length()==0){
-           return true;
-       }
-    
-        return palindromeCheck(str);
     }
 };
