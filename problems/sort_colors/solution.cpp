6bcebf7361a6f1103856ae0c4fb80e8d5aa07227
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2022-07-12 13:29:29 +0000

    Sync LeetCode submission - Sort Colors (cpp)

--- problems/sort_colors/solution.cpp
+++ problems/sort_colors/solution.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int low = 0 , mid = 0 ,high = nums.size()-1;
-        while(mid<=high){
-            if(nums[mid]==0){
-                swap(nums[mid++],nums[low++]);
+        int low = 0 , itr = 0 , high = nums.size()-1;
+        // use low as filling index of 0, high as filling index of 2's and itr to move through the array
+        while( itr<=high ){
+            if(nums[itr]==1){
+                itr++;
             }
-            else if(nums[mid]==1)mid++;
+            else if(nums[itr]==0){
+                swap(nums[itr++],nums[low++]);
+            }
             else {
-                swap(nums[mid],nums[high--]);
+                swap(nums[itr],nums[high--]);
             }
         }
-        
     }
 };
