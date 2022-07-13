f9eec74aa6e8593c347885898446e4b095e69748
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2022-07-13 14:28:57 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (cpp)

--- problems/binary_tree_level_order_traversal/solution.cpp
+++ problems/binary_tree_level_order_traversal/solution.cpp
@@ -13,22 +13,22 @@ class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
         if(!root)return {};
-        queue<TreeNode *>q;
+        queue<TreeNode*>q;
+        vector<vector<int>>res;
         q.push(root);
-        vector<vector<int>>result;
-        while(!q.empty()){
-            int n=q.size();
-            vector<int>temp;
+        while(q.empty()==false){
+            
+            vector<int>v;
+            int n = q.size();
             for(int i=0;i<n;i++){
-                auto curr = q.front();
+                auto temp = q.front();
                 q.pop();
-                temp.push_back(curr->val);
-                if(curr->left)q.push(curr->left);
-                if(curr->right)q.push(curr->right);
+                v.push_back(temp->val);
+                if(temp->left)q.push(temp->left);
+                if(temp->right)q.push(temp->right);
             }
-            result.push_back(temp);
-            temp.clear();
+            res.push_back(v);
         }
-        return result;        
+        return res;
     }
 };
