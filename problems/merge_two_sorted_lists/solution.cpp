bdf5e3ec04e4906544fdffaa874ec2687d787c93
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2022-07-14 09:29:19 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (cpp)

--- problems/merge_two_sorted_lists/solution.cpp
+++ problems/merge_two_sorted_lists/solution.cpp
@@ -12,41 +12,35 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         if(!list1 and !list2)return list1;
-        else if(!list1 and list2)return list2;
-        else if(!list2 and list1)return list1;
-        else{
-             ListNode *t=list1,*p=list2,*head=NULL,*tail=NULL;
-                while(t and p){
-                    if(t->val <= p->val){
-                        if(!head){
-                            head = t;
-                            tail = t;
-                            t=t->next;
-                        }
-                        else{
-                            tail->next=t;
-                            t=t->next;
-                            tail=tail->next;
-                        }
-                    }
-                    else{
-                    if(!head){
-                        head = p;
-                        tail = p;
-                        p=p->next;
-                    }
-                    else{
-                        tail->next = p;
-                        tail=tail->next;
-                        p=p->next;
-                    }
+        if(!list1 and list2)return list2;
+        if(list1 and !list2)return list1;
+        ListNode* itr1 = list1, *itr2 = list2,*head = NULL,*tail=NULL;
+        while(itr1 and itr2){
+            // always store smaller elements on list1;
+            int data1 = itr1->val , data2 = itr2->val;
+            if(data1<=data2){
+                auto next = itr1->next;
+                if(!head)head=itr1;
+                if(!tail)tail=itr1;
+                else {
+                    tail->next = itr1;
+                    tail = tail->next;
                 }
+                itr1 = next;
+            }
+            else{
+                auto next = itr2->next;
+                if(!head)head=itr2;
+                if(!tail)tail=itr2;
+                else{
+                    tail->next = itr2;
+                    tail = tail->next;
+                }
+                itr2=next;
+            }
         }
-        if(!t and !p)return head;
-        else if(!p and t)tail->next = t;
-        else if(!t and p)tail->next = p;
+        if(!itr1)tail->next = itr2;
+        if(!itr2)tail->next = itr1;
         return head;
-        }
-        
     }
 };
