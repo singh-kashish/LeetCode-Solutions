089cba94a8c03acde47dded154f51973f9f493ba
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2022-07-22 12:45:31 +0000

    Sync LeetCode submission - Intersection of Two Linked Lists (cpp)

--- problems/intersection_of_two_linked_lists/solution.cpp
+++ problems/intersection_of_two_linked_lists/solution.cpp
@@ -9,42 +9,29 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode * itr1 = headA, *itr2 = headB;
-        if(headA==headB)return headA;
-        if(headA and !headB)return NULL;
-        if(!headA and headB)return NULL;
-        int count1 = 1, count2 = 1;
-        while(itr1->next){
-            ++count1;
-            itr1 = itr1->next;
+        if(!headA or !headB)return NULL;
+        int lengthA = 0, lengthB = 0;
+        ListNode *temp = headA;
+        while(temp){
+            lengthA++;
+            temp = temp->next;
         }
-        while(itr2->next){
-            ++count2;
-            itr2 = itr2->next;
+        temp = headB;
+        while(temp){
+            lengthB++;
+            temp = temp->next;
         }
-        if(itr1!=itr2)return NULL;
+        ListNode * itrA = headA, *itrB = headB;
+        if(lengthA<=lengthB){
+            for(int i=0;i<lengthB-lengthA and itrB;i++)itrB=itrB->next;
+        }
         else {
-            if(count1>=count2){
-                itr1 = headA;
-                itr2 = headB;
-                for(int i=0;i<count1-count2;i++)itr1=itr1->next;
-                while(itr1!=itr2){
-                    itr1=itr1->next;
-                    itr2=itr2->next;
-                }
-                return itr1;
-            }
-            else{
-                itr1 = headA;
-                itr2 = headB;
-                for(int i=0;i<count2-count1;i++)itr2=itr2->next;
-                while(itr1!=itr2){
-                    itr1=itr1->next;
-                    itr2=itr2->next;
-                }
-                return itr1;
-                
-            }
+            for(int i=0;i<lengthA-lengthB and itrA;i++)itrA = itrA->next;
         }
+        while(itrA and itrB and itrA!=itrB){
+            itrA = itrA->next;
+            itrB = itrB->next;
+        }
+        return itrA;
     }
 };
