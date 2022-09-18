ef98d85b59daaae9807a0829a93293c6b5b1f373
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2022-09-18 11:57:02 +0000

    Sync LeetCode submission - Unique Paths (cpp)

--- problems/unique_paths/solution.cpp
+++ problems/unique_paths/solution.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         int dp[m][n];
-        memset(dp,0,sizeof(dp));
+        memset(dp,sizeof(dp),0);
         for(int i=0;i<m;i++)dp[i][0]=1;
-        for(int i=0;i<n;i++)dp[0][i]=1;
+        for(int j=0;j<n;j++)dp[0][j]=1;
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
                 dp[i][j]=dp[i-1][j]+dp[i][j-1];
