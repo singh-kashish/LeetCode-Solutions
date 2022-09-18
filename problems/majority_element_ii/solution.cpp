017c41ba5b3897cb905aa25c09cef9497767c434
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2022-09-18 11:40:09 +0000

    Sync LeetCode submission - Majority Element II (cpp)

--- problems/majority_element_ii/solution.cpp
+++ problems/majority_element_ii/solution.cpp
@@ -1,38 +1,18 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int>result;
-        unordered_map<int,int>m;
-         int n=nums.size();
-    //     for(int i=0;i<n;i++){
-    //         if(m.find(nums[i])!=m.end())m[nums[i]]++;
-    //         else if(m.size()<2){
-    //             m[nums[i]]=1;
-    //         }
-    //         else{
-    //             for(auto itr=m.begin();itr!=m.end();itr++){
-    //                 (itr->second)--;
-    //                 if(itr->second==0){
-    //                     auto p=itr;
-    //                     m.erase(p);
-    //                 }
-    //             }
-    //         }
-    //     }
-    //    for(auto x:m){
-    //     int count=0;
-    //     for(int i=0;i<n;i++){
-    //         if(x.first==nums[i])
-    //             count++;
-    //    }
-    //     if(count>floor(n/3))
-    //         result.push_back(x.first);
-    // }
-    //     return result;
-        for(int i=0;i<n;i++)m[nums[i]]++;
-        for(auto x:m){
-            if(x.second>floor(n/3))result.push_back(x.first);
+        vector<int>r;
+        int n = nums.size();
+        unordered_map<int,int>dict;
+        unordered_set<int>res;
+        for(auto x:nums){
+            dict[x]++;
+            if(dict[x]>n/3){
+                res.insert(x);
+            }
         }
-        return result;
+        
+        for(auto x:res)r.push_back(x);
+        return r;
     }
 };
