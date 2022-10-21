e2d2df47e1d65dc402a0767693f6be6461928db5
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2022-10-21 17:02:43 +0000

    Sync LeetCode submission - Contains Duplicate II (cpp)

--- problems/contains_duplicate_ii/solution.cpp
+++ problems/contains_duplicate_ii/solution.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        // i'd use a dict with sliding window to check if the value at right of window is present in the current window
-        if(nums.size()==0 or nums.size()==1)return false;
-        unordered_set<int>dict;
-        if(k>=nums.size()){
-            for(int i=0;i<nums.size();i++){
-                if(dict.find(nums[i])!=dict.end())return true;
-                dict.insert(nums[i]);    
+        unordered_map<int,unordered_set<int>>dict;
+        for(int i=0;i<nums.size();i++){
+            if(dict.find(nums[i])==dict.end()){
+                dict[nums[i]].insert(i);
             }
-            return false;
-        }
-        for(int i=0;i<=k;i++){
-            if(dict.find(nums[i])!=dict.end())return true;
-            dict.insert(nums[i]);
-        }
-        for(int i=k+1;i<nums.size();i++){
-            dict.erase(nums[i-k-1]);
-            if(dict.find(nums[i])!=dict.end())return true;
-            dict.insert(nums[i]);
+            else{
+                for(auto x:dict[nums[i]]){
+                    if(k>=abs(i-x))return true;
+                    else dict[nums[i]].insert(i);
+                }
+            }
         }
         return false;
     }
