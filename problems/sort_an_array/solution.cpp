18d0a1130e62a68d8e9974837a21e288f07c498a
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2022-10-24 08:11:16 +0000

    Sync LeetCode submission - Sort an Array (cpp)

--- problems/sort_an_array/solution.cpp
+++ problems/sort_an_array/solution.cpp
@@ -1,32 +1,47 @@
 class Solution {
 public:
-    void swap(int &a,int &b){
-        int temp=a;
-        a=b;
-        b=temp;
-    }
-    void heapify(vector<int> &num,int n,int i){
-        int largest=i,left=2*i+1,right=2*i+2;
-        if(left < n and num[left]>num[largest])largest=left;
-        if(right<n and num[right]>num[largest])largest=right;
-        if(i!=largest){
-            swap(num[largest],num[i]);
-            heapify(num,n,largest);
+    void merge(vector<int>&nums,int start,int mid,int end){
+        int n1=mid-start+1;
+        int n2=end-mid;
+        vector<int>left(n1);
+        vector<int>right(n2);
+        for(int i=0;i<n1;i++){
+            left[i]=nums[i+start];
         }
-    }
-    //build heap
-    void buildHeap(vector<int>&num,int n){
-        for(int i=(n-2)/2;i>=0;i--){
-            heapify(num,n,i);
+        for(int j=0;j<n2;j++){
+            right[j]=nums[j+mid+1];
         }
+        int i=0,j=0,itr=start;
+        while(i<n1 and j<n2){
+            if(left[i]<=right[j]){
+                nums[itr++]=left[i++];
+            }
+            else{
+                nums[itr++]=right[j++];
+            }
+        }
+        while(i<n1){
+            nums[itr++]=left[i++];
+        }
+        while(j<n2){
+            nums[itr++]=right[j++];
+        }
+        left.clear();
+        right.clear();
+    }
+    void mergeSort(vector<int>&nums,int start,int end){
+        if(end>start){
+            int mid=start+(end-start)/2;
+            mergeSort(nums,start,mid);
+            mergeSort(nums,mid+1,end);
+            merge(nums,start,mid,end);
+        }
+        else return;
+        
     }
     vector<int> sortArray(vector<int>& nums) {
-        int n=nums.size();
-        buildHeap(nums,n);
-        for(int i=n-1;i>=0;i--){
-            swap(nums[i],nums[0]);
-            heapify(nums,i,0);
-        }
+        int start =0 , end = nums.size()-1;
+        mergeSort(nums,start,end);
         return nums;
     }
 };
