d0d01777fd3fbeedb479c7901be30a31b3a983ea
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2022-12-14 10:26:05 +0000

    Sync LeetCode submission - Populating Next Right Pointers in Each Node (cpp)

---
+++ problems/populating_next_right_pointers_in_each_node/solution.cpp
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root==NULL)return root;
+        queue<Node*>q;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                Node* front = q.front();
+                q.pop();
+                if(front and q.size()!=0 and n-1!=i)front->next=q.front();
+                else if(front){
+                    front->next=NULL;
+                }
+                if(front and front->left)q.push(front->left);
+                if(front and front->right)q.push(front->right);
+            }
+        }
+        return root;
+    }
+};
