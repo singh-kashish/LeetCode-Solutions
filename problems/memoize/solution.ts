e2728807f2d66048fbbd77e6e9547c349ce763d0
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2024-04-07 12:59:40 +0000

    Sync LeetCode submission - Memoize (typescript)

---
+++ problems/memoize/solution.ts
@@ -0,0 +1,29 @@
+type Fn = (...params: number[]) => number
+
+function memoize(fn: Fn): Fn {
+    let memoizeSet = new Map();
+    
+    return function(...args):number {
+       let key:string = JSON.stringify(args);
+    if(memoizeSet.has(key)){
+        return memoizeSet.get(key);
+    }
+    else{
+        let ans:number = fn(...args);
+        memoizeSet.set(key , ans);
+        return ans;
+    }
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
