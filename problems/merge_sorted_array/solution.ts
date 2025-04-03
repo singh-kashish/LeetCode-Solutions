b8a1833df7c1982d760d8d9d27c1ca2df78ae4ff
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2025-04-03 15:24:21 +0000

    Sync LeetCode submission - Merge Sorted Array (typescript)

--- problems/merge_sorted_array/solution.ts
+++ problems/merge_sorted_array/solution.ts
@@ -1,26 +1,22 @@
 /**
  Do not return anything, modify nums1 in-place instead.
  */
- function swap(arr1:number[],arr2:number[],a:number,b:number){
-    let store:number = arr1[a];
-    arr1[a]=arr2[b];
-    arr2[b]=store;
-    return;
- }
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    let itr1:number =0, itr2:number=0;
-    if(n===0)return;
-    while(itr1<m){
-        if(nums1[itr1]<=nums2[itr2])itr1++;
-        else if(nums1[itr1]>nums2[itr2]){
-            swap(nums1,nums2,itr1,itr2);
-            nums2.sort((x:number,y:number)=>x-y);
-            itr1++;
+    let i : number = 0;
+    let j : number = 0;
+    while(i<m && j<n){
+        if(nums1[i]<=nums2[j])i++;
+        else {
+            let temp = nums1[i];
+            nums1[i]=nums2[j];
+            nums2[j]=temp;
+            nums2.sort((x,y)=>x-y);
+            i++;
         }
     }
-    while(itr1<m+n && itr2<n){
-        nums1[itr1]=nums2[itr2];
-        itr1++;
-        itr2++;
-    }
+    while(i<m+n && j<n){
+        nums1[i]=nums2[j];
+        i++;
+        j++;
+    } 
 };
