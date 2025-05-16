625144f9bd89f7075b87a44676611f37f288bffd
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2025-05-16 20:58:13 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock (typescript)

--- problems/best_time_to_buy_and_sell_stock/solution.ts
+++ problems/best_time_to_buy_and_sell_stock/solution.ts
@@ -1,10 +1,9 @@
 function maxProfit(prices: number[]): number {
-    let maxProfit:number = 0;
-    let minPrice:number = Number.MAX_SAFE_INTEGER;
-    for(let i : number = 0; i<prices.length; i++){
-        if(prices[i]<=minPrice)minPrice=prices[i];
-        maxProfit = Math.max(maxProfit,prices[i]-minPrice);
+    let minPrice = Infinity;
+    let maxProf = 0;
+    for(let price of prices){
+        if(price<minPrice)minPrice=price;
+        maxProf = Math.max(maxProf,price-minPrice);
     }
-    return maxProfit;
-    
+    return maxProf;
 };
