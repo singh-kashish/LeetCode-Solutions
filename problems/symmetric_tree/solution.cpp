a7706742b84aa0aa13a17d2e7b26e9ba325b5787
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2025-08-05 12:01:49 +0000

    Sync LeetCode submission - Symmetric Tree (cpp)

--- problems/symmetric_tree/solution.cpp
+++ problems/symmetric_tree/solution.cpp
@@ -11,12 +11,45 @@
  */
 class Solution {
 public:
-    bool checksame(TreeNode *left,TreeNode *right){
-        if(left==NULL and right==NULL) return true;
-        if(!left || !right)return false;
-        return ((left->val == right->val) and checksame(left->left,right->right) and  checksame(left->right,right->left));
-    }
     bool isSymmetric(TreeNode* root) {
-        return checksame(root,root);
+        if(!root)return false;
+       // return isMirror(root->left,root->right);
+       // Using Iteration we can do this using a stack or queue but adds space complexity -> Queue based Solution below:
+    //    queue<TreeNode*>q;
+    //    q.push(root->left);
+    //    q.push(root->right);
+    //    while(!q.empty()){
+    //     auto l = q.front();q.pop();
+    //     auto r = q.front();q.pop();
+    //     if(!l && !r)continue;
+    //     if(!l || !r)return false;
+    //     if(l->val!=r->val)return false;
+    //     if(l->val==r->val){
+    //         q.push(l->left);
+    //         q.push(r->right);
+    //         q.push(l->right);
+    //         q.push(r->left);
+    //     }
+    //    }
+    //    return true;
+    // }
+
+    //Stack solution below: -
+    stack<pair<TreeNode*,TreeNode*>>st;
+    st.emplace(root->left,root->right);
+    while(!st.empty()){
+        auto [l,r] = st.top();st.pop();
+        if(!l && !r)continue;
+        if(!l || !r || l->val != r->val)return false;
+        st.emplace(l->left,r->right);
+        st.emplace(l->right,r->left);
     }
-};
+    return true;
+
+    //Recursive Solution Below
+    // bool isMirror(TreeNode *left, TreeNode *right){
+    //     if(!left && !right)return true;
+    //     if(!left || !right)return false;
+    //     if(left->val!=right->val)return false;
+    //     return isMirror(left->left,right->right) && isMirror(right->left,left->right);
+}};
