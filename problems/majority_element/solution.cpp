e59d98ea4070ba31ccff0cb6210bae364c5e6ba4
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2025-08-05 17:07:26 +0000

    Sync LeetCode submission - Majority Element (cpp)

--- problems/majority_element/solution.cpp
+++ problems/majority_element/solution.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int>dict;
-        int n = nums.size();
-        for(auto x:nums){
-            dict[x]++;
-            if(dict[x]>n/2)return x;
-        }
-        return -1;
+        // Since there always will be a number which appears in array exactly more than size of array/2, we can use Boyer-Moore Voting Algorith here to maintain a candidate and count, if count is zero set current element as the candidate and change the count-> if current element is candidate then add 1 to count else subtract 1 from the count. Since it's guranteed to have 1 element appearing more than n/2 times -> This is the most optimal solution
+        int count = 0, candidate=0;
+        for( int x : nums ){
+            if(count==0)candidate=x;
+            count += (candidate==x)?1:-1;
+        } 
+        return candidate;
     }
 };
