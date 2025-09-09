c0ec74f7845849fd3e75dc842fff2998f621bb82
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2025-09-09 13:33:23 +0000

    Sync LeetCode submission - Next Greater Element I (cpp)

--- problems/next_greater_element_i/solution.cpp
+++ problems/next_greater_element_i/solution.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>result;
-        for(auto x:nums1){
-            auto itr=find(nums2.begin(),nums2.end(),x);
-            int q=itr-nums2.begin();
-         
-            bool vla=false;
-            for(int i=q+1;i<nums2.size();i++){
-                if(nums2[i]>x){
-                    result.push_back(nums2[i]);
-                    vla=true;
-                    break;
-                }
+        // Using monotonic stack and hashMap to store next greater element
+        vector<int>res;
+        unordered_map<int,int>nextGreater;
+        stack<int>st;
+        for(auto x:nums2){
+            while(!st.empty() && st.top()<x){
+                nextGreater[st.top()]=x;
+                st.pop();
             }
-            if(vla==false)result.push_back(-1);
+            st.push(x);
         }
-        return result;
+        while(!st.empty()){
+            nextGreater[st.top()]=-1;
+            st.pop();
+        }
+        for(auto i:nums1){
+            res.push_back(nextGreater[i]);
+        }
+        return res;
     }
 };
