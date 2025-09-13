e5f75ed7248104d763ab4b93e9608abfbc520877
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2025-09-13 13:53:00 +0000

    Sync LeetCode submission - Happy Number (cpp)

--- problems/happy_number/solution.cpp
+++ problems/happy_number/solution.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        unordered_set<int>s;
-        while(s.find(n)==s.end()){
-            s.insert(n);
-            int nn=0;
-            while(n>0){
-                int p=n%10;
-                nn+=p*p;
-                n/=10;
-            }
-            if(nn==1)return true;
-            n=nn;
+        int slow = n;
+        int fast = n;
+        do{
+            slow = squareOfDigits(slow);
+            fast = squareOfDigits(squareOfDigits(fast));
+            if(fast==1)return true; 
+        } while(slow!=fast);
+        return slow==1;
+    }
+    int squareOfDigits(int n){
+        int sum = 0;
+        while(n>0){
+            int digit = n%10;
+            sum+=digit*digit;
+            n = n/10;
         }
-        return false;
+        return sum;
     }
 };
