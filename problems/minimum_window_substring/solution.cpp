47a0efd188db343b3f0ab8f9ddcfc050e1e2493a
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2025-09-15 11:39:18 +0000

    Sync LeetCode submission - Minimum Window Substring (cpp)

--- problems/minimum_window_substring/solution.cpp
+++ problems/minimum_window_substring/solution.cpp
@@ -1,30 +1,41 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-         if(s.length()==0 or t.length()==0 or s.length()<t.length())return "";
-            
-            unordered_map<char,int>dict;
-            for(auto x:t)dict[x]++;
-            
-            vector<int>ans{-1,10000000};
-            int left = 0, right = 0 ,count = dict.size();
-            while(right<s.length()){
-                char curr = s[right];
-                if(dict.find(curr)!=dict.end()){
-                    dict[curr]--;
-                    if(dict[curr]==0)count--;
+        if(s.empty() || t.empty())return "";
+        unordered_map<char,int>need,window;
+        for(char c:t){
+            need[c]++;
+        }
+        int required = need.size();
+        int left=0,right=0;
+        int minLength = INT_MAX,minLeft = 0;
+        int formed=0;
+        while(right<s.size()){
+            char c = s[right];
+            window[c]++;
+            // If current char is in need and the count in need and window hash maps of char is same increment formed
+            if(need.count(c) && window[c]==need[c]){
+                formed++;
+            }
+            // Shrink window from left till required size is same as formed size
+            while(left<=right && formed==required){
+                // Update Minimum Window length and minLeft
+                if(right-left+1<minLength){
+                    minLength = right-left+1;
+                    minLeft=left;
                 }
-                while(count==0){
-                    if(right-left<ans[1]-ans[0])ans = {left,right};
-                     char curr = s[left];
-                    if(dict.find(curr)!=dict.end())dict[curr]++;
-                    if(dict.find(curr)!=dict.end() and dict[curr]>0)count++;
-                    left++;
+                char d = s[left];
+                window[d]--;
+                // Decrement Count of Formed if char is in need and count of this char in need is more than in window 
+                if(need.count(d) && need[d]>window[d]){
+                    formed--;
                 }
-                 right++;
-                }
-            if(ans[0]==-1 and ans[1]==10000000)return "";
-            return s.substr(ans[0],ans[1]-ans[0]+1);
-           
+                // Move left pointer now
+                left++;
+            }
+            // Increase window size
+            right++;
+        }
+        return minLength == INT_MAX ? "" : s.substr(minLeft,minLength);
     }
 };
