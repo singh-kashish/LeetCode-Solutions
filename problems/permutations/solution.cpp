317112509f2cb2e0e3dd229eb172133a155a6c65
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2025-09-17 12:09:55 +0000

    Sync LeetCode submission - Permutations (cpp)

--- problems/permutations/solution.cpp
+++ problems/permutations/solution.cpp
@@ -2,11 +2,25 @@ class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>result;
-        sort(nums.begin(),nums.end());
-        do{
-            result.push_back(nums);
-        }
-        while(next_permutation(nums.begin(),nums.end()));
+        vector<bool>used(nums.size(),false);
+        vector<int>path;
+        backtrack(result, path, used, nums);
         return result;
     }
+private:
+    void backtrack(vector<vector<int>>&result, vector<int>&path, vector<bool>&used, vector<int>&nums){
+        if(path.size()==nums.size()){
+            result.push_back(path);
+            return;
+        }
+        for(int i = 0 ; i<nums.size(); i++){
+            if(used[i])continue;
+            path.push_back(nums[i]);
+            used[i] = true;
+            backtrack(result,path,used,nums);
+            used[i]=false;
+            path.pop_back();
+        }
+        return;
+    }   
 };
