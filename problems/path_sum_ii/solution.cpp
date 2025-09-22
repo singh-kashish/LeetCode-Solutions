a03de5ac1e321c95fac970afeea1f16686513c4f
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2025-09-22 14:40:50 +0000

    Sync LeetCode submission - Path Sum II (cpp)

--- problems/path_sum_ii/solution.cpp
+++ problems/path_sum_ii/solution.cpp
@@ -11,27 +11,21 @@
  */
 class Solution {
 public:
-    void solve(TreeNode *root,int targetSum,int currSum,vector<vector<int>>&res,vector<int>&store){
-        if(!root)return;
-        if(currSum+root->val==targetSum and root->left==NULL and root->right==NULL){
-            store.push_back(root->val);
-            res.push_back(store);
-            if(root->left)solve(root->left,targetSum,currSum+root->val,res,store);
-            if(root->right)solve(root->right,targetSum,currSum+root->val,res,store);
-            store.pop_back();
-        }
-        else{
-            store.push_back(root->val);
-            if(root->left)solve(root->left,targetSum,currSum+root->val,res,store);
-            if(root->right)solve(root->right,targetSum,currSum+root->val,res,store);
-            store.pop_back();
-        }
-    }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        if(root==NULL)return {};
         vector<vector<int>>res;
-        vector<int>store;
-        solve(root,targetSum,0,res,store);
+        vector<int>currPath;
+        dfs(res,currPath,root,0,targetSum);
         return res;
+    }
+    void dfs(vector<vector<int>>&res, vector<int>arr, TreeNode* node, int currSum, int targetSum){
+        if(!node)return;
+        currSum+=node->val;
+        arr.push_back(node->val);
+        if(!node->left && !node->right && currSum == targetSum){
+            res.push_back(arr);
+        }
+        dfs(res,arr, node->left, currSum,targetSum);
+        dfs(res,arr, node->right, currSum,targetSum);
+        return;
     }
 };
