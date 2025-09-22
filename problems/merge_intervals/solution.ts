edad5d813152f48c713ce186aee318f5c085d0ca
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2025-09-22 14:00:01 +0000

    Sync LeetCode submission - Merge Intervals (typescript)

--- problems/merge_intervals/solution.ts
+++ problems/merge_intervals/solution.ts
@@ -1,14 +1,13 @@
 function merge(intervals: number[][]): number[][] {
+    let result = new Array<Array<number>>();
     intervals.sort((a,b)=>a[0]-b[0]);
-    let res : Array<Array<number>> = [intervals[0]];
-    for(let i:number = 1;i<intervals.length;i++){
-        const lastInserted : Array<number> = res[res.length-1];
-        const thisItem : Array<number> = intervals[i];
-        if(lastInserted[1]>=thisItem[0]){
-            lastInserted[1] = Math.max(lastInserted[1],thisItem[1]);
+    result.push(intervals[0]);
+    for(let itr=1;itr<intervals.length;itr++){
+        if(intervals[itr][0]<=result[result.length-1][1]){
+            result[result.length-1][1]=Math.max(intervals[itr][1],result[result.length-1][1]);
         } else{
-            res.push(intervals[i]);
+            result.push(intervals[itr]);
         }
     }
-    return res;
+    return result;
 };
