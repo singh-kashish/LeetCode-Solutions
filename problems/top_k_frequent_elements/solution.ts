89bcf4d826a23ae2a4651c1487f8ae1b388fcc85
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2025-09-26 13:17:37 +0000

    Sync LeetCode submission - Top K Frequent Elements (typescript)

--- problems/top_k_frequent_elements/solution.ts
+++ problems/top_k_frequent_elements/solution.ts
@@ -1,15 +1,34 @@
 function topKFrequent(nums: number[], k: number): number[] {
-    let res: number[]=[];
-    let freq = new Map<number,number>();
+    // let res: number[]=[];
+    // let freq = new Map<number,number>();
+    // for(const x of nums){
+    //     freq.set(x,(freq.get(x)||0)+1);
+    // }
+    // const arr = Array.from(freq.entries())
+    // .sort((a,b)=>b[1]-a[1]);
+    // let itr=0;
+    // while(k--){
+    //     res.push(arr[itr][0]);
+    //     itr++;
+    // }
+    // return res;
+    
+    //Using Bucket Sort below:-
+    let freq= new Map<number,number>();
+    let n = nums.length;
     for(const x of nums){
         freq.set(x,(freq.get(x)||0)+1);
     }
-    const arr = Array.from(freq.entries())
-    .sort((a,b)=>b[1]-a[1]);
-    let itr=0;
-    while(k--){
-        res.push(arr[itr][0]);
-        itr++;
+    const buckets : number[][]=new Array(n+1).fill(null).map(x=>[]);
+    for(const [num,count] of freq){
+        buckets[count].push(num);
+    }
+    let res = new Array<number>();
+    for(let itr = n;itr>=0 && res.length<k;itr--){
+        for(const x of buckets[itr]){
+            res.push(x);
+            if(k==res.length)break;
+        }
     }
     return res;
 };
