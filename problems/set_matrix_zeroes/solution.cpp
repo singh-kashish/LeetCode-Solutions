0a774bc1e3eb7d16d6b8e1d826c88a04a7db1063
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2025-09-28 18:35:04 +0000

    Sync LeetCode submission - Set Matrix Zeroes (cpp)

--- problems/set_matrix_zeroes/solution.cpp
+++ problems/set_matrix_zeroes/solution.cpp
@@ -1,18 +1,42 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        unordered_set<int>toBeSetToZeroI,toBeSetToZeroJ;
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size();j++){
+        int m = matrix.size();
+        if(m==0)return;
+        int n = matrix[0].size();
+        bool zeroRow=false, zeroCol=false;
+        for(int i=0;i<m;i++){
+            if(matrix[i][0]==0){
+                zeroCol=true;
+                break;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(matrix[0][i]==0){
+                zeroRow=true;
+                break;
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
                 if(matrix[i][j]==0){
-                    toBeSetToZeroI.insert(i);toBeSetToZeroJ.insert(j);
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
                 }
             }
         }
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size();j++){
-                if(toBeSetToZeroI.find(i)!=toBeSetToZeroI.end() or toBeSetToZeroJ.find(j)!=toBeSetToZeroJ.end())matrix[i][j]=0;
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[0][j]==0||matrix[i][0]==0)matrix[i][j]=0;
             }
         }
-    }
+        if(zeroRow){
+            for(int i=0;i<n;i++)matrix[0][i]=0;
+        }
+        if(zeroCol){
+            for(int i=0;i<m;i++)matrix[i][0]=0;
+        }
+        return;
+          }
+    
 };
