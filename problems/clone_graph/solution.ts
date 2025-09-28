612b0897071b14131e3268d30143e874c719228d
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2025-09-28 14:42:12 +0000

    Sync LeetCode submission - Clone Graph (typescript)

--- problems/clone_graph/solution.ts
+++ problems/clone_graph/solution.ts
@@ -16,14 +16,14 @@
 function cloneGraph(node: _Node | null): _Node | null {
     if(!node)return null;
     let processed = new Map<_Node,_Node>();
-    function dfs(root:_Node):_Node{
-        if(processed.has(root))return processed.get(root)!;
-        const copy = new _Node(root.val);
-        processed.set(root,copy);
-        for(const x of root.neighbors){
-            copy.neighbors.push(dfs(x));
+    function dfs(r: _Node|null): _Node|null{
+        if(processed.has(r))return processed.get(r);
+        let copy = new Node(r.val,[]);
+        processed.set(r,copy);
+        for(let n of r.neighbors){
+            copy.neighbors.push(dfs(n));
         }
         return copy;
-    }
+    } 
     return dfs(node);
 };
