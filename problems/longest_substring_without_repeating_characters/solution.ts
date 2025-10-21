2168560f30eca896a6b07c949599a6f26e04ba79
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2025-10-21 17:46:00 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (typescript)

--- problems/longest_substring_without_repeating_characters/solution.ts
+++ problems/longest_substring_without_repeating_characters/solution.ts
@@ -1,31 +1,15 @@
 function lengthOfLongestSubstring(s: string): number {
-    // if(s.length===0)return 0;
-    // let maxLenSubWithoutDuplicates : number = 1;
-    // let lengthOfString:number = s.length;
-
-    // for(let i : number=0 ; i < s.length; i++){
-    //     let store = new Set<string>(s[i]);
-    //      for(let j : number = i+1; j<s.length;j++){
-    //         if(store.has(s[j]))break;
-    //         store.add(s[j]);
-    //         maxLenSubWithoutDuplicates = Math.max(maxLenSubWithoutDuplicates,store.size);
-    //      }
-    // }
-    // return maxLenSubWithoutDuplicates;
-    
-    // Sliding Window:
-    let len : number = s.length; 
-    if(len===0 ||len===1 )return len;
-    let st = new Set<string>();
-    let maxLen : number = 1;
-    let rightPtr = 0;
-    for(let i:number=0 ; i<len;i++){
-        while(st.has(s[i])){
-            st.delete(s[rightPtr]);
-            rightPtr++;
+    let store = new Set<string>();
+    let start =0;
+    let res=0;
+    for(let end=0;end<s.length;end++){
+        const currChar = s[end];
+        while(store.has(currChar)){
+            store.delete(s[start]);
+            start++;
         }
-            st.add(s[i]);
-            maxLen = Math.max(i-rightPtr+1,maxLen);
+        store.add(currChar);
+        res = Math.max(res,end-start+1); 
     }
-    return maxLen;
+    return res;
 };
