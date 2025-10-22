e721f15d8aec417f8757885b2e3692747ca0e2e8
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2025-10-22 09:14:35 +0000

    Sync LeetCode submission - Product of Array Except Self (typescript)

--- problems/product_of_array_except_self/solution.ts
+++ problems/product_of_array_except_self/solution.ts
@@ -1,20 +1,13 @@
 function productExceptSelf(nums: number[]): number[] {
-    let leftArray:number[] = Array(nums.length);
-    let rightArray:number[] = Array(nums.length);
-    leftArray[0]=nums[0];
-    let n:number=nums.length;
-    rightArray[n-1]=nums[n-1];
-    for(let itr=1;itr<nums.length;itr++){
-        leftArray[itr]=leftArray[itr-1]*nums[itr];
+    let left =1, right=1;
+    let res = new Array<number>(nums.length);
+    for(let itr=0;itr<nums.length;itr++){
+        res[itr]=left;
+        left*=nums[itr];
     }
-    for(let itr=n-2;itr>=0;itr--){
-        rightArray[itr]=rightArray[itr+1]*nums[itr];
-    }
-    let res:number[]=Array(nums.length);
-    res[0]=rightArray[1];
-    res[nums.length-1]=leftArray[nums.length-2];
-    for(let itr=1;itr<nums.length-1;itr++){
-        res[itr]=leftArray[itr-1]*rightArray[itr+1];
+    for(let itr=nums.length-1;itr>=0;itr--){
+        res[itr]*=right;
+        right*=nums[itr];
     }
     return res;
 };
