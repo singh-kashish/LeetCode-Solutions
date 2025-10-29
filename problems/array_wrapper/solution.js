68ac4be8667c1067fc24db349ae28d958e3a6519
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2025-10-29 10:16:02 +0000

    Sync LeetCode submission - Array Wrapper (javascript)

---
+++ problems/array_wrapper/solution.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+   this.nums = nums; 
+   this.total = 0 ;
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+    this.nums.forEach(x=>this.total+=x);
+    return this.total;
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+    return `[${this.nums}]`
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
