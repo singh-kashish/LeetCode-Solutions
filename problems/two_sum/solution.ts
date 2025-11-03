d165c2898d3034d54576e866908946827e15f693
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2025-11-03 09:48:21 +0000

    Sync LeetCode submission - Two Sum (typescript)

--- problems/two_sum/solution.ts
+++ problems/two_sum/solution.ts
@@ -1,11 +1,17 @@
 function twoSum(nums: number[], target: number): number[] {
     if(nums.length<2)return [];
-    let hash : Map<number,number> = new Map();
-    hash.set(nums[0],0);
-    for(let i=1;i<nums.length;i++){
-        let ele:number = nums[i];
-        if(hash.has(target-ele))return [i,hash.get(target-ele)];
-        else hash.set(ele,i);
+    type pair={
+        val:number;
+        index:number;
+    }
+    let withIndex : pair[] = nums.map((val,index)=>({val,index}));
+    withIndex.sort((a,b)=>a.val-b.val);
+    let left =0, right=withIndex.length-1;
+    while(left<right){
+        const sum = withIndex[left].val+withIndex[right].val;
+        if(sum==target)return [withIndex[left].index,withIndex[right].index];
+        else if(sum>target)right--;
+        else left++;
     }
     return [-1,-1];
 };
