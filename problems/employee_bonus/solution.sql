37cc5d8503e5921be1c498db0008c8e895d696a6
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2025-11-11 14:47:22 +0000

    Sync LeetCode submission - Employee Bonus (mysql)

--- problems/employee_bonus/solution.sql
+++ problems/employee_bonus/solution.sql
@@ -1,5 +1,8 @@
-SELECT e.name, b.bonus
-FROM Employee AS e
-LEFT JOIN Bonus AS b
-  ON e.empId = b.empId
-WHERE COALESCE(b.bonus, -1) < 1000;
+# Write your MySQL query statement below
+SELECT e.name as name, b.bonus as bonus from Employee AS e LEFT join Bonus AS b ON e.empId = b.empId
+where COALESCE(b.bonus,-1)<1000; 
+-- SELECT e.name, b.bonus
+-- FROM Employee AS e
+-- LEFT JOIN Bonus AS b
+--   ON e.empId = b.empId
+-- WHERE COALESCE(b.bonus, -1) < 1000;
